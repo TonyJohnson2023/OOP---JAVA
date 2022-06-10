7a05448ad3342ad4375d5c47fc158eb9003e5d95
TonyJohnson2023 <107246652+TonyJohnson2023@users.noreply.github.com>
2022-06-10 15:08:18 +0530

    Add files via upload
    Course Outcome 3(CO3):
    1. Area of different shapes using overloaded functions.

---
+++ AreaOverload.java
@@ -0,0 +1,24 @@
+class AreaShape {
+void area(int x){
+    System.out.println("Area of square is="+(x*x));
+}
+void area(int x, int y){
+    System.out.println("Area. of rectangle is ="+(x*y));
+}
+void area(int x,int y, int z){
+    double s=(x+y+z)/2;
+    double triArea;
+    triArea = Math.sqrt(s*(s-x)*(s-y)*(s-z));
+    System.out.println("Area of triangle is ="+triArea);
+ }
+}
+public class AreaOverload{
+    
+    public static void main(String[] args) {
+    AreaShape obj = new AreaShape();
+obj.area(10);
+obj.area(5,6);
+obj.area(4,5,6);
+ }
+}
+    
